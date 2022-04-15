ae56e424737577448699c38df578a787db042ee3
Яков <yakovzubets@gmai.com>
2022-04-15 14:05:03 +0300

    convert audio to video waves

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-### script
+from email.mime import audio
+import ffmpeg
+
+audio= ffmpeg.input('easy.mp3')
+
+(
+    ffmpeg
+    .input('easy.mp3')
+    .filter('showwaves', s='hd720', mode='cline', r='25', scale='sqrt', colors='#000080|#006400')
+    .output(audio.audio, 'out.mp4', pix_fmt='yuv420p')
+    .overwrite_output()
+    .run()
+)
+
