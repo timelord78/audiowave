e69bf145c41febff50ff0e481c3be4aba0ab0751
Яков <yakovzubets@gmai.com>
2022-04-18 09:30:47 +0300

    func created

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from timeit import repeat
 import ffmpeg
 import random
 import os
 
 
-def generate_colors(n):
+# function creating different colors for audiowave in HEX format
+# arg - quantity of parts
+def generate_colors(q):
     colors = []
     r = lambda: random.randint(0, 255)
-    for i in range(2*n):
+    for i in range(2*q):
         colors.append('#%02X%02X%02X' % (r(), r(), r()))
     return colors
 
 
-# ffmpeg -i easy.mp3 -f segment -segment_time 2  out%03d.mp3
-def segmentation(n):
-    os.system(f'ffmpeg -i easy.mp3 -f segment -segment_time {n}  out%01d.mp3')
+# ffmpeg command for division audio file in parts
+# (duration - specified N seconds)
+def segmentation(n, file_name):
+    os.system(f'ffmpeg -i {file_name} -f segment -segment_time {n}  out%01d.mp3')
 
 
+# ffmpeg-python script converting audio in audiowaves mp4 with different colors
 def showaves(q):
     colors = generate_colors(q)
     for i in range(q):
@@ -25,7 +28,9 @@ def showaves(q):
             (
                 ffmpeg
                 .input(f'out{i}.mp3')
-                .filter('showwaves', s='hd720', mode='cline', r='25', scale='sqrt', colors=f'{colors.pop()}|{colors.pop}')
+                .filter(
+                    'showwaves', s='hd720', mode='cline', r='25',
+                    scale='sqrt', colors=f'{colors.pop()}|{colors.pop}')
                 .output(audio.audio, f'out{i}.mp4', pix_fmt='yuv420p')
                 .overwrite_output()
                 .run()
@@ -36,41 +41,32 @@ def showaves(q):
             f.write(f'file out{i}.mp4\n')
 
 
-#  ffmpeg -f concat -i text.txt -c copy result.mp4    
+#  ffmpeg command to union all mp4 files in one
 def concat():
     os.system('ffmpeg -f concat -i concat.txt -c copy result.mp4')
 
+
+# ffmpeg command to set gif file as a background
 def background_gif():
     os.system('ffmpeg -i result.mp4 -ignore_loop 0 -i bg.gif -filter_complex "[1:v]scale=1280:-1,crop=iw:720[bg]; \
-              [0:v]scale=800:-1[fg];[bg][fg]overlay=10:10:shortest=1" -q:v 3 final.mp4')
-    # try:
-    #     (
-    #         ffmpeg
-    #         .input('result.mp4')
-    #         .overlay(gif, eof_action=)
-    #         .output(wave.audio, 'final.mp4')
-    #         .overwrite_output()
-    #         .run()
-    #     )
-    # except Exception as e:
-    #     print(e)
-
-# ffmpeg -i input.mp4 -ignore_loop 0 -i input.gif -filter_complex overlay=10:10:shortest=1 out.mkv
+              [0:v]scale=800:-1[fg];[bg][fg]overlay=240:125:shortest=1" -q:v 3 final.mp4')
 
 
 if __name__ == '__main__':
-    # print('Введите как часто менять цвет волны (секунды)')
-    # n = int(input())
-    # segmentation(n)
-    # files = os.listdir()
-    # q = 0
-    # for el in files:
-    #     if el.startswith('out'):
-    #         q += 1
-    # showaves(q)
-    # concat()
-    # os.remove('concat.txt')
-    # for el in os.listdir():
-    #     if el.startswith('out'):
-    #         os.remove(el)
+    print('Введите как часто менять цвет волны (секунды)')
+    n = int(input())
+    print('Введите название исходного аудио файла (как пример - test.mp3)')
+    file_name = input()
+    segmentation(n, file_name)
+    files = os.listdir()
+    q = 0
+    for el in files:
+        if el.startswith('out'):
+            q += 1
+    showaves(q)
+    concat()
+    os.remove('concat.txt')
+    for el in os.listdir():
+        if el.startswith('out'):
+            os.remove(el)
     background_gif()
